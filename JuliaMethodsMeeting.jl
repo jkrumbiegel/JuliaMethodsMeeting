3b89f594cf5937c799ee7af886707055bc9b71ce
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-02-12 12:02:13 +0100

    update

--- JuliaMethodsMeeting.jl
+++ JuliaMethodsMeeting.jl
@@ -1,13 +1,32 @@
+"""
+An abstract representation of the result of one trial
+"""
 struct Trial{S, T}
-    decision::S
-    result::T
+    decision::S # what did the observer decide?
+    result::T # and what result did the decision bring?
 end
 
 
 function run_trial(strategy, game, previous_trials)
     decision = decide(strategy, game, previous_trials)
     result = determine_result(game, decision)
+    # learn here
+    # learn!(strategy, game, decision, result)
     Trial(decision, result)
+end
+
+function decide(strategy, game, previous_trials)
+    error("""
+        No decide method yet for strategy $(typeof(strategy)) and game $(typeof(game)).
+        You have to create one!)
+    """)
+end
+
+function determine_result(game, decision)
+    error("""
+        No determine_result method yet for game $(typeof(game)) and decision $(typeof(decision)).
+        You have to create one!)
+    """)
 end
 
 function run_experiment(strategy, game, ntrials)
@@ -25,28 +44,64 @@ function run_experiment(strategy, game, ntrials)
 end
 
 
+
+
+
+
+
+
+"""
+A game with N slots that each have a winning probability between 0 and 1.
+"""
 struct SlotsGame{N}
     probs::NTuple{N, Float64}
 end
 
+SlotsGame((0.0, 1.0))
+SlotsGame((0.0, 0.3, 0.7))
+
+slotsgame = SlotsGame((0.2, 0.8))
+
+
+
+
+
+
+
+
 struct RandomStrategy end
 
 
+run_experiment(RandomStrategy(), slotsgame, 1_000)
+
 decide(::RandomStrategy, ::SlotsGame{N}, previous_trials) where N = rand(1:N)
 
-determine_result(g::SlotsGame, decision::Int) = rand() > g.probs[decision]
+run_experiment(RandomStrategy(), slotsgame, 1_000)
 
-random_results = run_experiment(RandomStrategy(), SlotsGame((0.2, 0.8)), 1_000)
+determine_result(g::SlotsGame, decision::Int) = rand() <= g.probs[decision]
 
-using StatsBase
+random_results = run_experiment(RandomStrategy(), slotsgame, 1_000)
+
 
 decision(t::Trial) = t.decision
 result(t::Trial) = t.result
 
+using StatsBase
+
 countmap(decision.(random_results))
 
 
+
+
+
+
+
+
+
 struct SwitchOnLose end
+
+run_experiment(SwitchOnLose(), slotsgame, 1_000)
+
 
 function decide(::SwitchOnLose, ::SlotsGame{N}, previous_trials) where N
 
@@ -54,27 +109,41 @@ function decide(::SwitchOnLose, ::SlotsGame{N}, previous_trials) where N
         return rand(1:N)
     end
 
-    p = previous_trials[end]
-    if result(p) == true
-        decision(p)
+    lasttrial = previous_trials[end]
+    if result(lasttrial) == true
+        decision(lasttrial)
     else
-        options = tuple((i for i in 1:N if i != decision(p))...)
+        options = tuple((i for i in 1:N if i != decision(lasttrial))...)
         rand(options)
     end
 end
 
-sol_results = run_experiment(SwitchOnLose(), SlotsGame((0.2, 0.8)), 1000)
+sol_results = run_experiment(SwitchOnLose(), slotsgame, 1000)
 
 countmap(decision.(sol_results))
 
-struct ChooseBestSoFar end
 
-function decide(::ChooseBestSoFar, ::SlotsGame{N}, previous_trials) where N
+
+
+
+
+
+
+
+struct ChooseBestInLastN
+    n::Int
+end
+
+function decide(strategy::ChooseBestInLastN, ::SlotsGame{N}, previous_trials) where N
 
     choicecounts = zeros(Int, N)
     successes = zeros(Int, N)
 
-    for t in previous_trials
+    nprevious = length(previous_trials)
+    nback = strategy.n
+    indices = max(1, nprevious - nback):nprevious
+    for i in indices
+        t = previous_trials[i]
         choicecounts[t.decision] += 1
         if t.result == true
             successes[t.decision] += 1
@@ -85,18 +154,69 @@ function decide(::ChooseBestSoFar, ::SlotsGame{N}, previous_trials) where N
     best = argmax(successratios)
 end
 
-bsf_results = run_experiment(ChooseBestSoFar(), SlotsGame((0.2, 0.8)), 1000);
+bsf_results = run_experiment(ChooseBestInLastN(10), slotsgame, 1_000);
 
 countmap(decision.(bsf_results))
 
 
-results = [run_experiment(st, SlotsGame(probs), 1000)
-    for st in (ChooseBestSoFar(), RandomStrategy(), SwitchOnLose()),
-        probs in [(0.2, 0.8), (0.5, 0.5), (1/3, 1/3, 1/3)]
-];
 
 
-countmap.(decision.(r) for r in results)
+
+
+
+
+
+
+using DataFrames
+using FilteredGroupbyMacro
+using Statistics
+
+df = join(
+    DataFrame(
+        game = [SlotsGame((0.2, 0.8)), SlotsGame((0.4, 0.6)), SlotsGame((0.5, 0.5)), SlotsGame((0.9, 0.6, 0.3))]),
+    DataFrame(
+        strategy = [
+            RandomStrategy(), SwitchOnLose(), ChooseBestInLastN(10), ChooseBestInLastN(3)
+        ]),
+    DataFrame(
+        run = 1:100
+    ),
+    kind = :cross
+)
+
+resultdf = @by df[!, [:game, :strategy, :run],
+    trial = run_experiment(:strategy[1], :game[1], 1000)]
+
+resultdf[:result] = result.(resultdf[:trial])
+resultdf[:decision] = decision.(resultdf[:trial])
+
+
+avg_results = @by resultdf[!, [:game, :strategy, :run],
+        p_win = mean(:result)
+    ][!, [:game, :strategy],
+        mean_p_win = mean(:p_win), sd_p_win = std(:p_win)
+    ]
+
+
+using Plots
+using StatsPlots
+
+@df avg_results groupedbar(
+    string.(:game),
+    :mean_p_win,
+    yerr = :sd_p_win,
+    group = string.(:strategy),
+    ylabel = "Percentage of successes",
+    xrotation = 30,
+    ylims = (0, 1),
+    legend = :top,
+    title = "Games and strategies")
+
+
+
+
+
+
 
 using Distributions
 
@@ -116,11 +236,11 @@ function decide(::SwitchOnLose, dslots::DistributionSlots{N}, previous_trials) w
         return rand(1:N)
     end
 
-    p = previous_trials[end]
-    if result(p) > 0
-        decision(p)
+    previous = previous_trials[end]
+    if result(previous) > 0
+        decision(previous)
     else
-        options = tuple((i for i in 1:N if i != decision(p))...)
+        options = tuple((i for i in 1:N if i != decision(previous))...)
         rand(options)
     end
 end
